c810333cf8fddaa5ab60ee1a3a3127c9881edda7
Arnfinn <arnfinn.steindal@gmail.com>
2017-11-08 13:35:22 +0100

    Travis, started documentation with sphinx etc.
    Rearranged lag_format.py to be more python-like.
    Made it pep8

---
+++ index.rst
@@ -0,0 +1,26 @@
+.. SKDE python scripts documentation master file, created by
+   sphinx-quickstart on Wed Nov  8 11:13:13 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Dokumentasjon av SKDE sine python scripts
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
+lag_format.py
+=============
+
+.. automodule::lag_format
+    :members:
--- lag_format.py
+++ lag_format.py
@@ -2,70 +2,76 @@
 # -*- coding: utf-8 -*-
 
 
-import sys
-import argparse
-from subprocess import call
+def lagFormat():
+    """Script for å lage sas-format-filer ut fra csv-fil TEST"""
 
-parser = argparse.ArgumentParser(description='Konvertere tekstfil til SAS format-fil')
+    import sys
+    import argparse
+    from subprocess import call
 
-parser.add_argument('-i', dest='innfil',
-                    help='Tekstfilen som skal konverteres')
+    parser = argparse.ArgumentParser(description='Konvertere tekstfil til SAS format-fil')
 
-parser.add_argument('-o', dest='utfil',
-                    help='Produsert SAS-formatfil')
+    parser.add_argument('-i', dest='innfil',
+                        help='Tekstfilen som skal konverteres')
 
-parser.add_argument('--behold', dest='behold', action='store_true',
-                    default=False,
-                    help='Behold også koden/første ord i format-teksten (standard: %(default))')
+    parser.add_argument('-o', dest='utfil',
+                        help='Produsert SAS-formatfil')
 
-parser.add_argument('--delim', dest = 'delim', 
-                    default = '',
-                    help = 'Mellomrom mellom ord i inputfil (standard er mellomrom)')
+    parser.add_argument('--behold', dest='behold', action='store_true',
+                        default=False,
+                        help='Behold også koden/første ord i format-teksten (standard: %(default))')
 
-args = parser.parse_args()
+    parser.add_argument('--delim', dest='delim',
+                        default='',
+                        help='Mellomrom mellom ord i inputfil (standard er mellomrom)')
 
-filename = args.innfil
-alle = args.behold
-utfil = args.utfil
+    args = parser.parse_args()
 
-infile = open(filename,"r")
+    filename = args.innfil
+    alle = args.behold
+    utfil = args.utfil
 
-utTekst = "proc format;\n"
-utTekst += "value $"+filename.split(".")[0]+"F\n"
+    infile = open(filename, "r")
 
-k = 0
-for i in infile.readlines():
-    linje = i.replace('\t'," ")
-    k += 1
-    try:
-        if args.delim != "":
-            words = linje.split(args.delim)
-        else:
-            words = linje.split()
-        if alle:
-            text = linje.rstrip()
-        else:
-            text = " ".join(words[1:]).rstrip()
-        newline = "'"+words[0].replace(" ", "")+"'"+'="'+text.replace('"',"'")+'"\n'
-        utTekst += newline
-    except:
-        sys.stderr.write(i)
+    utTekst = "proc format;\n"
+    utTekst += "value $" + filename.split(".")[0] + "F\n"
 
-infile.close()
+    k = 0
+    for i in infile.readlines():
+        linje = i.replace('\t', " ")
+        k += 1
+        try:
+            if args.delim != "":
+                words = linje.split(args.delim)
+            else:
+                words = linje.split()
+            if alle:
+                text = linje.rstrip()
+            else:
+                text = " ".join(words[1:]).rstrip()
+            newline = "'" + words[0].replace(" ", "") + "'" + '="' + text.replace('"', "'") + '"\n'
+            utTekst += newline
+        except IndexError:
+            sys.stderr.write(i)
 
-utTekst += '''
+    infile.close()
+
+    utTekst += '''
 ;
 run
 '''
 
-#utTekst = utTekst.encode("cp1252")
+    # utTekst = utTekst.encode("cp1252")
 
-utfil = open(args.utfil, "w", encoding='cp1252')#, newline='\r\n')
-utfil.write(utTekst)
-utfil.close()
+    utfil = open(args.utfil, "w", encoding='cp1252')  # , newline='\r\n')
+    utfil.write(utTekst)
+    utfil.close()
+
+    # call(["iconv","-f","utf-8","-t","cp1252","--unicode-subst=''",args.utfil,">","tmp"], shell=True)
+    # call(["mv","tmp",args.utfil])
+    # call(["rm","tmp"])
 
 
-#call(["iconv","-f","utf-8","-t","cp1252","--unicode-subst=''",args.utfil,">","tmp"], shell=True)
-#call(["mv","tmp",args.utfil])
-#call(["rm","tmp"])
-
+if __name__ == '__main__':
+    """ TEST """
+    lag_format()
