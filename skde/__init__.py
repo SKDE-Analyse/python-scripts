845dbc394d84de9c804fc06e73adfe65e3dfc208
Arnfinn <arnfinn.steindal@gmail.com>
2018-05-22 15:03:02 +0200

    fix of pycodestyle error

--- skde/__init__.py
+++ skde/__init__.py
@@ -1,6 +1,6 @@
-__all__ = ['lag_format', 
-           'legg_til_nye_formater', 
-           'sort_npr-format', 
+__all__ = ['lag_format',
+           'legg_til_nye_formater',
+           'sort_npr-format',
            'sort_npr-melding',
            'extract_sas_documentation',
            'extract_hdiag3tegn']
